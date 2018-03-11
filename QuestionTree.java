5701c5b689b3c78fb070f5607ee6143f25166a7e
MM212 <19749908+MM212@users.noreply.github.com>
2018-03-11 11:29:20 -0700

    V1.0.1
    Doesn't work

--- QuestionTree.java
+++ QuestionTree.java
@@ -1,36 +1,47 @@
 import java.util.*;
+import java.io.*;
 
 public class QuestionTree {
    
    private int totalGames;
    private int gamesWon;
    private QuestionNode overallRoot;
+   private UserInterface ui;
    
    //Pre: 
    //Post:
-   public Question Tree(UserInterface ui) {
-
+   public QuestionTree(UserInterface ui) {
+      if (ui == null) throw new IllegalArgumentException("Method passed null parameter");
+      
       totalGames = 0;
       gamesWon = 0;
-      overallRoot = new QuestionNode("Computer");
+      overallRoot = new QuestionNode("A:computer");
+      this.ui = ui;
    }
    
+   //
    //Pre: 
    //Post: 
    public void play() {
-      
+      totalGames++;
+      overallRoot = player(overallRoot);     
    }
    
    //Pre: 
    //Post:
    public void save(PrintStream output) {
+      if (output == null) throw new IllegalArgumentException("Method passed null parameter");
+      
       saver(output, overallRoot);
    }
    
    //Pre: 
    //Post:
    public void load(Scanner input) {
-      loader(input, overallRoot);      
+      if (input == null) throw new IllegalArgumentException("Method passed null parameter");
+      
+      overallRoot = null;
+      overallRoot = loader(input, overallRoot);      
    }
    
    //Post: Returns value of totalGames
@@ -43,15 +54,43 @@ public class QuestionTree {
       return gamesWon;
    }
    
-   //Pre: Untested
+   //Pre:
    //Post:
-   private void saver(PrintStream output, QuestionNode input) {
-      output.println(input.data);
-      saver(output, root.left);
-      saver(output, root.left);
+   private QuestionNode player(QuestionNode root) {
+      
+      String s = root.data.substring(2);
+      if (root.data.charAt(0) == 'A') {
+          ui.print("Would your object happen to be " + s + "?");
+          if(ui.nextBoolean()) {
+            gamesWon++;
+          } else {
+            ui.println("I lose.  What is your object? ");
+            String a = ui.nextLine();
+            ui.println("Type a yes/no question to distinguish your item from " + s + ": ");
+            String q = ui.nextLine();
+            ui.println("What is the answer for your object? ");
+            boolean d = ui.nextBoolean();
+            return adder(q, a, d, root);
+          }
+      } else {
+         if(ui.nextBoolean()) {
+            root.left = player(root.left);
+         } else {
+            root.right = player(root.right);
+         }
+      }
+      return root;
    }
    
    //Pre: Untested
+   //Post:
+   private void saver(PrintStream output, QuestionNode root) {      
+      output.println(root.data);
+      if (root.left != null) saver(output, root.left);
+      if (root.right != null) saver(output, root.right);
+   }
+   
+   //Pre: Worked in test, needs load() to clear overallRoot first
    //Post:
    private QuestionNode loader(Scanner input, QuestionNode root) {
       if (!input.hasNext()) return null;
@@ -68,8 +107,20 @@ public class QuestionTree {
       return root;
    }
    
-   private QuestionNode add() {
-   
+//    Do after player()
+//    Pre:
+//    Post: 
+   private QuestionNode adder(String q, String a, boolean d, QuestionNode root) {
+      QuestionNode temp = root;
+      String que = "Q:" + q;
+      String ans = "A:" + a;
+      QuestionNode newTerm = new QuestionNode(ans);
+      if (d) {
+         root = new QuestionNode(que, newTerm, temp);
+      } else {
+         root = new QuestionNode(que, temp, newTerm);
+      }
+      return root;
    }
 
 }
