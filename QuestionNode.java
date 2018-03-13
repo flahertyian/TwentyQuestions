79b83c94403d112fdf46f17b17f2919766bba4b8
MM212 <19749908+MM212@users.noreply.github.com>
2018-03-13 13:00:20 -0700

    V1.0.3

--- QuestionNode.java
+++ QuestionNode.java
@@ -1,14 +1,19 @@
+//For use with QuestionTree and QuestionMain
 public class QuestionNode {
    
+   //Fields for data and branches
    public String data;
    public QuestionNode left;//yes
    public QuestionNode right;//no
-
-
+   
+   //Pre: Accepts data value
+   //Post: Initializes terminal node
    public QuestionNode(String input) {
       this.data = input;
    }
    
+   //Pre: Accepts data and branch values (blank must be null)
+   //Post: Initializes non-terminal node
    public QuestionNode(String input, QuestionNode yes, QuestionNode no) {
       this.data = input;
       this.left = yes;
